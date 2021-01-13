317b6a6cb9774c572278b0a69901444fbfc48cc8
s1410722020 <71477268+s1410722020@users.noreply.github.com>
2021-01-13 11:44:47 +0800

    2021.1.13

--- EndTermserver.js
+++ EndTermserver.js
@@ -2,15 +2,17 @@
 
 var express = require("express");
 var formidable = require("formidable");
-var fs = require("fs");
 server = express();
 var bodyParser = require("body-parser");
 var sizeOf = require("image-size")
 var Datastore = require('nedb');
+var fs = require("fs");
 var Announcement = new Datastore({ filename: __dirname + '/FinalTermdata/update.db', autoload: true });
 var Users = new Datastore({ filename: __dirname + '/FinalTermdata/usersresponse.db', autoload: true });
 
-  // var userData = [
+var Photos = new Datastore({ filename: __dirname + '/FinalTermdata/standings.db', autoload: true, });
+
+// var userData = [
 // {"title":"試看看布告欄底下功能","date":"2020/12/22","img":"test","content":"內容測試測試，希望能成功"}];
 //  {"title":"測試分頁1","date":"2021/1/7","img":"test","content":"分頁加油啊1"},
 //  {"title":"測試分頁2","date":"2021/1/7","img":"test","content":"分頁加油啊2"},
@@ -23,7 +25,9 @@ var Users = new Datastore({ filename: __dirname + '/FinalTermdata/usersresponse.
 // Announcement.insert(userData);
 
 // var userData2 = [
-//   {"name":"蛋包飯","content2":"訪客回饋專區測試"}
+//   {"name":"Doe","content2":"測試測試","like":0,"dislike":0},
+//   {"name":"已經怠惰的蛋包飯","content2":"持續不斷的測試","like":0,"dislike":0}
+
 
 // ];
 // Users.insert(userData2);
@@ -41,7 +45,7 @@ server.set("views", __dirname + "/FinalTermviews")
 server.post("/submitForm", function (req, res) {
 
 });
-server.post("/response", function (req, res) {
+server.post("/response", function (req, res) {//問題回報
   var form = formidable.IncomingForm();
   form.maxFileSize = 500 * 1024;
   form.parse(req, function (err, fields, files) {
@@ -104,24 +108,26 @@ server.post("/response", function (req, res) {
 var count = Users.count;
 // <%! int cnt = 0; %>;
 
-server.post("/visitormessage", function (req, res) {
+server.post("/visitormessage", function (req, res) {//訪客回饋，即時更新到討論區
   // Users.find({}, function (err, res) {
   //   if (err == null) {
   count++;
   console.log(count);
+  req.body.like = Number(req.body.like);
   Users.insert(req.body);
   res.render("success", { success: "Success Send", next: "VisitorMessagePage.html" });
   //   }
   // });
 });
 
-server.post("/Manegerupdate", function (req, res) {
-    // var fileExt = req.body.img.split(".")[1];
-   req.body.img = req.body.img.split(".")[0];
-    Announcement.insert(req.body);
-    res.render("success", { success: "Success Send", next: "ManagerUpdate.html" });
+server.post("/Manegerupdate", function (req, res) {//視覺化後臺管理公告用網頁
+  // var fileExt = req.body.img.split(".")[1];
+  req.body.img = req.body.img.split(".")[0];
+  req.body.dislike = Number(req.body.dislike);
+  Announcement.insert(req.body);
+  res.render("success", { success: "Success Send", next: "ManagerUpdate.html" });
 
-  });
+});
 
 
 // var AdmZip = require('adm-zip');
@@ -153,17 +159,17 @@ server.post("/Manegerupdate", function (req, res) {
 //   })
 // })
 
-server.get("/announcement", function (req, res) {
+server.get("/announcement", function (req, res) {//公告，更新日誌
 
   Announcement.find({}).sort({ date: -1 }).exec(function (err, result) {
     if (err == null) {
-       res.render("announcement", { list: result });
+      res.render("announcement", { list: result });
       // res.send({list:result});
     }
   });
 });
 
-server.get("/update", function (req, res) {
+server.get("/update", function (req, res) {//公告，更新日誌點進公告標題後
   // var form = formidable.IncomingForm();
   // Announcement.find({}).sort({date:-1}).exec(function (err, result){
   //   if (err == null) {
@@ -180,13 +186,55 @@ server.get("/update", function (req, res) {
   // });
 });
 
-server.get("/forum", function (req, res) {
-  Users.find({}, function (err, result) {
+server.get("/forum", function (req, res) {//討論區
+  Users.find({}).sort({ like: -1 }).exec(function (err, result) {
     if (err == null) {
       res.render("Forum", { response: result });
     }
   });
 });
+
+server.post('/cute', function (req, res) {//拿之前cute貓檔案改的 同意投票
+  Users.findOne({ _id: req.body.id }, function (err, id) {
+    if (err == null) {
+      var Original = id.like;
+      Users.update({ _id: req.body.id }, { $set: { like: Original + 1 } }, function (err, doc) {
+        if (err == null) {
+          //res.redirect("/");
+          Users.update({ _id: req.body.id }, { $addToSet: { votes: id.name } }, { upsert: true }, function () {
+            if (err == null)
+              Users.find({}, function (err, result) {
+                if (err == null) {
+                  res.render("Forum", { response: result });
+                }
+              });
+          })
+        }
+      });
+    }
+  })
+})
+server.post('/notcute', function (req, res) {//拿之前cute貓檔案改的 不同意投票
+  Users.findOne({ _id: req.body.id }, function (err, id) {
+    if (err == null) {
+      var Original = id.dislike;
+      Users.update({ _id: req.body.id }, { $set: { dislike: Original + 1 } }, function (err, doc) {
+        if (err == null) {
+          //res.redirect("/");
+          Users.update({ _id: req.body.id }, { $addToSet: { votes: id.name } }, { upsert: true }, function () {
+            if (err == null)
+              // res.redirect("/");
+              Users.find({}, function (err, result) {
+                if (err == null) {
+                  res.render("Forum", { response: result });
+                }
+              });
+          })
+        }
+      });
+    }
+  })
+})
 
 server.get("/", function (req, res) {
   //res.send("Hello World!");
